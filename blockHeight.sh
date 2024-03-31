a5e5d100cb899bcf3f282630e7b7628d785eaeca
amis13 <lindoespinoso@gmail.com>
2024-03-31 04:03:04 +0100

    updating blockHeight.sh

--- blockHeight.sh
+++ blockHeight.sh
@@ -14,7 +14,7 @@ blockHeight="$(docker logs -n 10 kroma-node | awk '/l2_engineSyncTarget/ {print}
 
 if [ "$blockHeight" ]; then
 
-	docker logs -n 10 kroma-node | awk '/l2_engineSyncTarget/ {print}' | sed 's/ \+/\n/g' | awk '/^l2_engineSyncTarget/ {print}' | tr -s ':' ' ' | awk 'NF{print $NF}' > blocks.txt
+	docker logs -n 30 kroma-node | awk '/l2_engineSyncTarget/ {print}' | sed 's/ \+/\n/g' | awk '/^l2_engineSyncTarget/ {print}' | tr -s ':' ' ' | awk 'NF{print $NF}' > blocks.txt
 	echo -e "\n${greenColour}[+]${endColour} ${grayColour}Block height: ${yellowColour}$(tail -n 1 blocks.txt)${endColour}\n"
 	rm blocks.txt
 else
